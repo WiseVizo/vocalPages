5a41355612b721f3936c0dac094c659c809a5cfa
WiseVizo <mecoc1011@gmail.com>
2024-11-22 23:10:08 +0530

    installing requirements

--- v1/database.py
+++ v1/database.py
@@ -1,10 +1,11 @@
-from sqlalchemy.engine import create_engine
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.engine import create_engine # type: ignore
+from sqlalchemy.orm import sessionmaker, declarative_base # type: ignore
+# from sqlalchemy.ext.declarative import declarative_base #type: ignore
 
-# URL_DATABASE = 'mysql+pymysql://root:root@localhost:3306/fastapidb'
-URL_DATABASE = 'mysql+pymysql://rootuser:test1234!@fastapi-aws-database.cfmamgu4ss9g.ap-south-1.rds.amazonaws.com:3306/fastapidb'
+URL_DATABASE = 'mysql+pymysql://root:root@localhost:3306/mydb'
+# URL_DATABASE = 'mysql+pymysql://rootuser:test1234!@fastapi-aws-database.cfmamgu4ss9g.ap-south-1.rds.amazonaws.com:3306/fastapidb'
 engine = create_engine(URL_DATABASE)
 sesion_local = sessionmaker(autoflush=False, autocommit=False, bind=engine)
 
-base = declarative_base()  
+base = declarative_base()
+print("hey")
